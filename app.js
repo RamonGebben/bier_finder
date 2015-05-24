5135abf6638ee59df494a9454a7660b5ef5623ad
Ramon <ramon@ra-ge.net>
2015-05-24 13:02:36 -0400

    Server prepare

--- app.js
+++ app.js
@@ -35,11 +35,4 @@ app.get('/breweries', function( req, res ){
 	});
 });
 
-var server = app.listen( 3000, function() {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Beerfinder is listening at http://%s:%s', host, port);
-
-});
+app.listen(6789);
